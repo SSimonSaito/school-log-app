fbc890f4aa2677d3dfc34bdc6f3a8e87aa3d761e
しんちゃん <159669389+SSimonSaito@users.noreply.github.com>
2025-06-15 16:24:20 +0900

    Add files via upload

--- 1_Homeroom.py
+++ 1_Homeroom.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pandas as pd
 from datetime import datetime
 from google_sheets_utils import (
