2fb7b45a86764ae1aa626754b6761ddee3c0e27d
しんちゃん <159669389+SSimonSaito@users.noreply.github.com>
2025-06-15 16:43:50 +0900

    Update 1_Homeroom.py

--- 1_Homeroom.py
+++ 1_Homeroom.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import pandas as pd
 from datetime import datetime
 from google_sheets_utils import (
